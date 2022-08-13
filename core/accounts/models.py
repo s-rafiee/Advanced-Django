0cd5871bd75ef1b6e8c90e7545585749b5e93cd2
saman <saman.rafiee.rs@google.com>
2022-08-13 23:03:36 +0430

    create account app and create user model

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
 
 # Create your models here.
+class User(AbstractBaseUser):
+    """
+    Custom User Model for our app.
+    """
+    email = models.EmailField(max_length=255, unique=True)
+    # The password was defined by default in AbstractBaseUser.
+    first_name = models.CharField(max_length=256, null=True)
+    last_name = models.CharField(max_length=256, null=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_verified = models.BooleanField(default=False)
+    image = models.ImageField(blank=True, null=True, upload_to='users/%Y/%m/%d/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
