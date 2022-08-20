3082d8927a6acc540055cd68c0e45a6371cb47ff
saman <saman.rafiee.rs@google.com>
2022-08-20 17:35:15 +0430

    add user permission and group

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 appname = 'blog'
 urlpatterns = [
     path('categories/', views.CategoryView.as_view(), name='categories'),
+    path('categories/create/', views.CreateCategoryFormView.as_view(), name='create_categories'),
+    path('categories/test/', views.test, name='testcat'),
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+# from django.views.generic import ListView, FormView
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 from . import models
+from . import forms
+from django.shortcuts import HttpResponse, reverse
+from django.urls import reverse
 
 
 # Create your views here.
 class CategoryView(ListView):
     template_name = 'blog/categories.html'
     model = models.Category
+    paginate_by = 15
+    ordering = ['-id']
+
+
+class CreateCategoryFormView(CreateView):
+    models = models.Category
+    form_class = forms.CategoryForm
+    template_name = 'blog/create_categories.html'
+    success_url = '/dashboard/categories/'
+
+
+def test(request):
+    cat = models.Category(title='t1', slug='s1')
+    cat.save()
+    return HttpResponse('ss')
